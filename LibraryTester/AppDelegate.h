4b516fafd51933d1c8d9484f6f11b2fbc537dea7
Zhigang Gao <gzg1984@aliyun.com>
2018-03-03 19:10:25 +0800

    Initial Commit

---
+++ LibraryTester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LibraryTester
+//
+//  Created by 高志刚 on 2018/3/3.
+//  Copyright © 2018年 高志刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
